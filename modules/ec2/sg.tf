14f211d71573c5d7d1490b41e63bf74226075464
maruruyeon <maruruyeon@forcs.com>
2025-09-18 10:13:15 +0900

    infra:atlantis test

--- modules/ec2/sg.tf
+++ modules/ec2/sg.tf
@@ -30,3 +30,13 @@ resource "aws_security_group_rule" "allowGithub" {
     cidr_blocks = ["0.0.0.0/0"]
     security_group_id = aws_security_group.bastion.id 
 }
+
+
+resource "aws_security_group_rule" "allowtest" {
+    type = "ingress"
+    from_port = "4142"
+    to_port = "4142"
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.bastion.id 
+}
