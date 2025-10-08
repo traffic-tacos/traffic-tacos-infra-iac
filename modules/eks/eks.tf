d1d422209cb505b39a1ccebc124f2ddf9ff91a11
maruruyeon <maruruyeon@forcs.com>
2025-10-08 21:15:17 +0900

    monitoring node수 증가

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -272,9 +272,9 @@ resource "aws_eks_node_group" "monitoring_node_group" {
   }
 
   scaling_config {
-    desired_size = 1
-    min_size     = 1
-    max_size     = 1
+    desired_size = 2
+    min_size     = 2
+    max_size     = 3
   }
 
   labels = {
