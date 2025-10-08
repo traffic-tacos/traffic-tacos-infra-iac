7f466f004f8e6ab6cef83eae2ee92e84bd1871cc
manyb2ns <manyb2ns@gmail.com>
2025-10-08 20:12:53 +0900

    infra: WAF 부하테스트 UA 예외처리 추가

--- modules/waf/main.tf
+++ modules/waf/main.tf
@@ -36,20 +36,40 @@ resource "aws_wafv2_web_acl" "cf_web_acl" {
         }
 
         # 테스트용 User-Agent 제외
+        # 테스트용 User-Agent 제외 (여러 개)
         scope_down_statement {
           not_statement {
             statement {
-              byte_match_statement {
-                search_string = "hey/0.0.1"
-                field_to_match {
-                  single_header {
-                    name = "user-agent"
+              or_statement {
+                statement {
+                  byte_match_statement {
+                    search_string = "hey/0.0.1"
+                    field_to_match {
+                      single_header {
+                        name = "user-agent"
+                      }
+                    }
+                    positional_constraint = "EXACTLY"
+                    text_transformation {
+                      priority = 0
+                      type     = "NONE"
+                    }
                   }
                 }
-                positional_constraint = "EXACTLY"
-                text_transformation {
-                  priority = 0
-                  type     = "NONE"
+                statement {
+                  byte_match_statement {
+                    search_string = "loadtester/1.0"
+                    field_to_match {
+                      single_header {
+                        name = "user-agent"
+                      }
+                    }
+                    positional_constraint = "EXACTLY"
+                    text_transformation {
+                      priority = 0
+                      type     = "NONE"
+                    }
+                  }
                 }
               }
             }
@@ -118,33 +138,41 @@ resource "aws_wafv2_web_acl" "cf_web_acl" {
       rate_based_statement {
         limit                 = 1000
         evaluation_window_sec = 60
-        aggregate_key_type    = "IP" # User-Agent 검사할 경우 "CUSTOM_KEYS"
-
-        # customKeys: (User-Agent, IP)로 버킷을 묶어 집계
-        # custom_key {
-        #   header {
-        #     name = "User-Agent"
-        #     text_transformation {
-        #       priority = 0
-        #       type     = "NONE"
-        #     }
-        #   }
-        # }
-
-        # custom_key {
-        #   ip {}
-        # }
+        aggregate_key_type    = "IP"
 
         scope_down_statement {
-          byte_match_statement {
-            search_string = "/" # 혹은 var.api_path_prefix
-            field_to_match {
-              uri_path {}
+          and_statement {
+            statement {
+              byte_match_statement {
+                search_string = "/" # or var.api_path_prefix
+                field_to_match {
+                  uri_path {}
+                }
+                positional_constraint = "STARTS_WITH"
+                text_transformation {
+                  priority = 0
+                  type     = "NONE"
+                }
+              }
             }
-            positional_constraint = "STARTS_WITH"
-            text_transformation {
-              priority = 0
-              type     = "NONE"
+            statement {
+              not_statement {
+                statement {
+                  byte_match_statement {
+                    search_string = "k6-load-test/1.0"
+                    field_to_match {
+                      single_header {
+                        name = "user-agent"
+                      }
+                    }
+                    positional_constraint = "EXACTLY"
+                    text_transformation {
+                      priority = 0
+                      type     = "NONE"
+                    }
+                  }
+                }
+              }
             }
           }
         }
@@ -154,7 +182,7 @@ resource "aws_wafv2_web_acl" "cf_web_acl" {
     visibility_config {
       cloudwatch_metrics_enabled = var.cloudwatch_metrics_enabled
       sampled_requests_enabled   = var.sampled_requests_enabled
-      metric_name                = "RBR_APIv1_UserAgentIP"
+      metric_name                = "RBR_APIv1_IP"
     }
   }
 
